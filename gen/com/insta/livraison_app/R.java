f791f288777e97e7f2cbd4c9e2f22f070a4390b9
Juan <juan.trochez@hotmail.fr>
2016-04-15 10:00:00 +0200

    mise en page login

--- gen/com/insta/livraison_app/R.java
+++ gen/com/insta/livraison_app/R.java
@@ -11,6 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        public static final int black=0x7f040006;
         public static final int darkBlue=0x7f040004;
         public static final int darkGreen=0x7f040002;
         public static final int darkGrey=0x7f040000;
@@ -34,19 +35,18 @@ public final class R {
         public static final int sloth2=0x7f020001;
     }
     public static final class id {
-        public static final int Envoyer=0x7f090006;
-        public static final int JsonTest=0x7f090007;
-        public static final int Login=0x7f090002;
-        public static final int Password=0x7f090003;
-        public static final int SaveData=0x7f090005;
-        public static final int ShowPassWord=0x7f090004;
-        public static final int action_settings=0x7f09000c;
-        public static final int detailTextView=0x7f09000a;
-        public static final int detail_fragment=0x7f090009;
-        public static final int dismiss=0x7f09000b;
-        public static final int imageView=0x7f090001;
-        public static final int list_fragment=0x7f090008;
-        public static final int textview=0x7f090000;
+        public static final int Envoyer=0x7f090005;
+        public static final int JsonTest=0x7f090006;
+        public static final int Login=0x7f090001;
+        public static final int Password=0x7f090002;
+        public static final int SaveData=0x7f090004;
+        public static final int ShowPassWord=0x7f090003;
+        public static final int action_settings=0x7f09000b;
+        public static final int detailTextView=0x7f090009;
+        public static final int detail_fragment=0x7f090008;
+        public static final int dismiss=0x7f09000a;
+        public static final int imageView=0x7f090000;
+        public static final int list_fragment=0x7f090007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -58,14 +58,15 @@ public final class R {
         public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int Envoyer=0x7f060005;
-        public static final int Login=0x7f060003;
-        public static final int Password=0x7f060004;
-        public static final int SaveData=0x7f060007;
-        public static final int ShowPassWord=0x7f060006;
-        public static final int action_settings=0x7f060002;
+        public static final int Envoyer=0x7f060006;
+        public static final int Login=0x7f060004;
+        public static final int Password=0x7f060005;
+        public static final int SaveData=0x7f060008;
+        public static final int ShowPassWord=0x7f060007;
+        public static final int action_settings=0x7f060003;
         public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060001;
+        public static final int hello_world=0x7f060002;
+        public static final int login=0x7f060001;
     }
     public static final class style {
         /** 
