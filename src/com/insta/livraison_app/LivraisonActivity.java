62033947ee5211326d6fcaac44fac0da4ce26b3a
userinsta <lebrazideck@hotmail.fr>
2016-04-15 17:25:21 +0100

    Service + BroadcastReceiver pour détecter quand l'utilisateur est connecté ou non à internet

--- src/com/insta/livraison_app/LivraisonActivity.java
+++ src/com/insta/livraison_app/LivraisonActivity.java
@@ -2,7 +2,12 @@ package com.insta.livraison_app;
 
 import java.util.concurrent.ExecutionException;
 
+import com.insta.livraison_app_Service.DetectConnection;
+
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.widget.CheckBox;
@@ -13,6 +18,12 @@ public class LivraisonActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.livraison);
+		
+		Intent intent = new Intent(LivraisonActivity.this,DetectConnection.class);
+		if(!isMyServiceRunning(DetectConnection.class))
+		{
+			startService(intent);
+		}
 		
 		JsonLoader getJsonConnection = new JsonLoader("livraison", findViewById(android.R.id.content), (CheckBox)findViewById(R.id.SaveData));
 		try {
@@ -28,5 +39,25 @@ public class LivraisonActivity extends FragmentActivity {
 	
 	public void onBackPressed() {
 		moveTaskToBack(true);
+	}
+	
+	private boolean isMyServiceRunning(Class<?> serviceClass) {
+	    ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+	    for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+	        if (serviceClass.getName().equals(service.service.getClassName())) {
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+	
+	public void onResume(){
+		super.onResume();
+
+		if(!isMyServiceRunning(DetectConnection.class))
+		{
+			Intent intent = new Intent(LivraisonActivity.this,DetectConnection.class);
+			startService(intent);
+		}		
 	}
 }
