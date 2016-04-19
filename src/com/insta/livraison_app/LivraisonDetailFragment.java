51f9d6b51e88cf8af7b00f3846db377919b803cd
userinsta <lebrazideck@hotmail.fr>
2016-04-19 17:00:25 +0100

    Refactor livraisonDetailFragment & icon for call and sms

--- src/com/insta/livraison_app/LivraisonDetailFragment.java
+++ src/com/insta/livraison_app/LivraisonDetailFragment.java
@@ -5,19 +5,20 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.telephony.PhoneStateListener;
-import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class LivraisonDetailFragment extends Fragment {
 	
 	public static int index;
-	private Button buttonCall;
+	
+	private ImageView callImage;
+	private ImageView smsImage;
 	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.livraison_detail, container, false);
@@ -40,69 +41,57 @@ public class LivraisonDetailFragment extends Fragment {
 		
 		
 		TextView tvNomClient = (TextView)getView().findViewById(R.id.nom_Client);
-		tvNomClient.setText(client.getNom());
+		tvNomClient.setText("Nom: ".concat(client.getNom()));
 		
 		TextView tvPrenomClient = (TextView)getView().findViewById(R.id.prenom_Client);
-		tvPrenomClient.setText(client.getPrenom());
+		tvPrenomClient.setText("Prenom: ".concat(client.getPrenom()));
 		
 		TextView tvEmailClient = (TextView)getView().findViewById(R.id.email_Client);
-		tvEmailClient.setText(client.getEmail());
+		tvEmailClient.setText("Email: ".concat(client.getEmail()));
 		
 		TextView tvAdresseClient = (TextView)getView().findViewById(R.id.adresse_Client);
-		tvAdresseClient.setText(livraison.getAdresse());
+		tvAdresseClient.setText("Adresse: ".concat(livraison.getNumero().concat(" ".concat(livraison.getAdresse().concat(" ".concat(livraison.getVille()))))));
+		
+		TextView tvStatutLivraison = (TextView)getView().findViewById(R.id.statut_Client);
+		if(livraison.getStatut() == 0)
+		{
+			tvStatutLivraison.setText("Statut: En cours");
+		}else if(livraison.getStatut() == 1)
+		{
+			tvStatutLivraison.setText("Statut: Terminé");
+		}	
 		
 		TextView tvTelephoneClient = (TextView)getView().findViewById(R.id.telephone_Client);
-		tvTelephoneClient.setText(client.getTelephone());
+		tvTelephoneClient.setText("Tel: ".concat(client.getTelephone()));
 		
-		buttonCall = (Button) getView().findViewById(R.id.buttonCall);
+		callImage = (ImageView) getView().findViewById(R.id.imageCall);
 		
-		// add PhoneStateListener
-//		PhoneCallListener phoneListener = new PhoneCallListener();
-//		TelephonyManager telephonyManager = (TelephonyManager) getActivity().getSystemService(Context.TELEPHONY_SERVICE);
-//		telephonyManager.listen(phoneListener,PhoneStateListener.LISTEN_CALL_STATE);
-		
-		buttonCall.setOnClickListener(new OnClickListener() {
+		callImage.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View arg0) {
 				Intent callIntent = new Intent(Intent.ACTION_CALL);
-				callIntent.setData(Uri.parse("tel:0661294672"));
+				callIntent.setData(Uri.parse("tel:".concat(client.getTelephone())));
 				startActivity(callIntent);
 			}
 		});
+		
+		smsImage = (ImageView) getView().findViewById(R.id.imageSms);
+		
+		smsImage.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				Intent callIntent = new Intent(Intent.ACTION_CALL);
+				callIntent.setData(Uri.parse("tel:".concat(client.getTelephone())));
+				startActivity(callIntent);
+			}
+		});
+		
 	}	
 	public void onResume()
 	{
 		super.onResume();
 		this.update(index);
 	}
-	
-//	private class PhoneCallListener extends PhoneStateListener {
-//
-//		private boolean isPhoneCalling = false;
-//
-//		@Override
-//		public void onCallStateChanged(int state, String incomingNumber) {
-//
-//
-//			if (TelephonyManager.CALL_STATE_OFFHOOK == state) {
-//				isPhoneCalling = true;
-//			}
-//
-//			if (TelephonyManager.CALL_STATE_IDLE == state) {
-//				// run when class initial and phone call ended, 
-//				// need detect flag from CALL_STATE_OFFHOOK
-//
-//				if (isPhoneCalling) {
-//					// restart app
-////					Intent i = getActivity().getBaseContext().getPackageManager().getLaunchIntentForPackage(getActivity().getBaseContext().getPackageName());
-////					i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-////					startActivity(i);
-//
-//					isPhoneCalling = false;
-//				}
-//			}
-//		}
-//	}
-
 }
